b9a1d4ecd56eb890506077efee828e5db1e8b34c
kynikov <hellboynikov@gmail.com>
2022-12-25 18:53:57 +0200

    Setup Program.cs to use DbContext

--- BulkyBookWeb/Data/ApplicationDbContext.cs
+++ BulkyBookWeb/Data/ApplicationDbContext.cs
@@ -4,12 +4,19 @@ using System.Data.Common;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BulkyBookWeb.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace BulkyBookWeb.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
 
+        }
+
+        public DbSet<Category> Categories { get; set; }
     }
 
     
